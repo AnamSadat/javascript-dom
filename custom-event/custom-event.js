07f153fec4811ba47a03ebc6ef6669c8f46f36bc
AnamSadat <anamsadat3@gmail.com>
2025-02-28 15:10:20 +0700

    add bubbling-capturing and form

--- custom-event/custom-event.js
+++ custom-event/custom-event.js
@@ -2,12 +2,16 @@ const changeCaption = new Event("changeCaption");
 
 window.addEventListener("load", function () {
   const tombol = document.getElementById("tombol");
+
   tombol.addEventListener("changeCaption", customEventHandler);
   tombol.addEventListener("click", function () {});
+  tombol.addEventListener("click", function () {
+    tombol.dispatchEvent(changeCaption);
+  });
 });
 
 function customEventHandler(ev) {
   console.log("event " + ev.type + " telah dijalankan");
-  const caption = getElementById("caption");
+  const caption = document.getElementById("caption");
   caption.inneText = "Anda telah membangkitkan custom event";
 }
