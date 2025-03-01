b4ee3784d2ad0538b30ab636341fb22ca9fef3eb
AnamSadat <anamsadat3@gmail.com>
2025-03-01 14:31:33 +0700

    add oncopy ang onpaste

--- input-event/form.js
+++ input-event/form.js
@@ -23,4 +23,53 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("notifikasiSisaKarakter").style.color = "black";
     }
   });
+
+  document.getElementById("inputNama").addEventListener("focus", function () {
+    console.log("inputNama: focus");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "visible";
+  });
+
+  document.getElementById("inputNama").addEventListener("blur", function () {
+    console.log("inputNama: blur");
+    document.getElementById("notifikasiSisaKarakter").style.visibility =
+      "hidden";
+  });
+
+  document
+    .getElementById("inputCaptcha")
+    .addEventListener("change", function () {
+      console.log("inputCaptcha: change");
+
+      const inputCaptcha = document.getElementById("inputCaptcha").value;
+      const submitButtonStatus = document.getElementById("submitButton");
+
+      if (inputCaptcha === "PRNU") {
+        submitButtonStatus.removeAttribute("disabled");
+      } else {
+        submitButtonStatus.setAttribute("disabled", "");
+      }
+    });
+
+  document
+    .getElementById("formDataDiri")
+    .addEventListener("submit", function (event) {
+      const inputCaptcha = document.getElementById("inputCaptcha").value;
+
+      if (inputCaptcha === "PRNU") {
+        alert("Selamat Datang Minna :D");
+      } else {
+        alert("Captcha anda belum tepat :(");
+        document.getElementById("submitButton").setAttribute("disabled", "");
+      }
+      event.preventDefault();
+    });
+
+  document.getElementById("inputCopy").addEventListener("copy", function () {
+    alert("Copy telah berhasil");
+  });
+
+  document.getElementById("inputPaste").addEventListener("paste", function () {
+    alert("Paste telah berhasil");
+  });
 });
